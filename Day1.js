ab0aa732d0a42d0db1119f2729bf564bccd1d3b1
Tanishka Gupta <tanishkagupta104@gmail.com>
2024-07-20 02:33:05 +0530

    Day1 file is added.

---
+++ Day1.js
@@ -0,0 +1,45 @@
+//activity 1:
+var x=23;
+console.log(x);
+let str="happy";
+console.log(str);
+//activity 2:
+const value=true;
+console.log(value);
+//activity 3 + Feature request 1:
+let name="Tanishka";
+console.log(typeof(name));
+console.log(name);
+let rollNo=121;
+console.log(typeof(rollNo));
+console.log(rollNo);
+let female=true;
+console.log(typeof(female));
+console.log(female);
+const person={fullName:"Tanishka Gupta",age:19};
+console.log(typeof(person));
+console.log(person);
+const colors=["black","blue","pink"];
+console.log(typeof(colors));
+console.log(colors);
+const date=new Date("2024-07-20");
+console.log(typeof(date));
+console.log(date);
+//activity 4:
+let marks=48;
+console.log(marks);
+marks=50;
+console.log(marks);
+//activity 5:
+// const accountNo=123456789;
+// accountNo=987654321;
+
+//Feature request 2:
+//>>bank details:
+const NAME="Bunny";
+const accountNumber=34567;
+let city="jhansi";
+
+//account number ans name are credentials which shouldnt be chnaged.
+//City or place of residence can be changed so it is initiated with let.
+
